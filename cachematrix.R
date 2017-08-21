57a7dd6b6b1607eafec14deb273d14ab7958754b
Sophia-Xiao <31077573+Sophia-Xiao@users.noreply.github.com>
2017-08-21 01:32:52 -0700

    Assigment solutions
    Tested all the functions in R Studio and it works.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,47 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+        invs <- NULL
+        
+        ## set is a function changes the matrix stored in the main function
+        set <- function(y) {
+                m <<- y
+                invs <<- NULL
+        }
+        ## return the matrix m stored in the main function        
+        get <- function() m
+        
+        ## store the inverse matrix 
+        setinvs <- function(solve) invs <<- solve
+        
+        ## return the inverse matrix
+        getinvs <- function() invs
+        
+        ## store the four functions in the function makeCasheMatrix
+        ## note: use $ operator to access each function from the list
+        list(set = set,
+             get = get,
+             setinvs = setinvs,
+             getinvs = getinvs)
 }
-
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...) {
+        
+        invs <- m$getinvs()
+        ## if reverse matrix exists in memory, return a message and inverse matrix
+        if(!is.null(invs)){
+                message("getting cached inverse matrix")
+                return(invs)
+        }
+        
+        ## gest matrix m and caculate inverse matrix
+        data <- m$get()
+        invs <- solve(data, ...)
+        m$setinvs(invs)
+        invs
 }
+
+## the end ##
